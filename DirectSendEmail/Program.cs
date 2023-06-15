6ac05f76c8ce1c31e667f95fb34c3d86a1b99339
Bobiskey <bob007@bob007.co.uk>
2023-06-15 14:20:52 +0100

    Add project files.

---
+++ DirectSendEmail/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Net.Mail;
+
+
+        var eTo = ConfigurationManager.AppSettings["emailTo"];
+        var eFrom = ConfigurationManager.AppSettings["emailFrom"];
+        var eHost = ConfigurationManager.AppSettings.Get("emailHost");
+        int ePort = Convert.ToInt32(ConfigurationManager.AppSettings.Get("emailPort"));
+
+        MailMessage alert = new MailMessage();
+        alert.To.Add(new MailAddress(eTo));
+        alert.From = new MailAddress(eFrom);
+        alert.Subject = "Email Direct Send";
+        alert.Body = "Demo of email direct send. <br/> Direct send requires no username or password. <br/> <br/> Direct send does however require the public IP address to be in the dmaain SPF record. <br/>";
+        alert.IsBodyHtml = true;
+
+        SmtpClient mail = new SmtpClient();
+        mail.Port = ePort;
+        mail.Host = eHost;
+        mail.EnableSsl = true;
+        mail.Send(alert);
